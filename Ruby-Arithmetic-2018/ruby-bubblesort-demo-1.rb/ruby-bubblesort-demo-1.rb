26fb44e52b050d6efbdf60a5b5a1685e882dec71
asm32cn <asm32@qq.com>
2018-05-29 01:49:57 +0800

    ruby-bubblesort-demo-1.rb ... modify

--- Ruby-Arithmetic-2018/ruby-bubblesort-demo-1.rb/ruby-bubblesort-demo-1.rb
+++ Ruby-Arithmetic-2018/ruby-bubblesort-demo-1.rb/ruby-bubblesort-demo-1.rb
@@ -20,11 +20,14 @@ class BubbleSortDemo1
     end
 end
 
+def main
+	# data = Array[41, 67, 34, 0, 69, 24, 78, 58, 62, 64, 5, 45, 81, 27, 61, 91, 95, 42, 27, 36]
+	data = Array[76, 11, 11, 43, 78, 35, 39, 27, 16, 55, 1, 41, 24, 19, 54, 7, 78, 69, 65, 82]
 
-# data = Array[41, 67, 34, 0, 69, 24, 78, 58, 62, 64, 5, 45, 81, 27, 61, 91, 95, 42, 27, 36]
-data = Array[76, 11, 11, 43, 78, 35, 39, 27, 16, 55, 1, 41, 24, 19, 54, 7, 78, 69, 65, 82]
+	bsd = BubbleSortDemo1.new
+	bsd.DisplayData data
+	bsd.BubbleSort data
+	bsd.DisplayData data
+end
 
-bsd = BubbleSortDemo1.new
-bsd.DisplayData data
-bsd.BubbleSort data
-bsd.DisplayData data
+main
